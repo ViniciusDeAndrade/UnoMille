e48e537eae848700e60ff83d9eece0eef64bb864
Marcos Vinicius de Araújo Andrade <viniciusclo@gmail.com>
2016-05-27 16:38:16 -0300

    comitando coisa pouca enquanto o resto não sai
    Naming

--- UnoMille/src/naming/NamingImpl.java
+++ UnoMille/src/naming/NamingImpl.java
@@ -2,29 +2,39 @@ package naming;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import services.ClientProxy;
 
 public class NamingImpl implements INaming{
 
-	
+
+	private Map<String, ClientProxy> mapaServicos = new HashMap<>();
+	private List<ClientProxy> listaServicos = new ArrayList<>();
+
 	public void bind(String serviceName, ClientProxy clientProxy) throws UnknownHostException, IOException, Throwable {
-
-		
+		mapaServicos.put(serviceName, clientProxy);
+		listaServicos.add(clientProxy);
 	}
 
-	
+
 	public ClientProxy lookup(String serviceName) throws UnknownHostException, IOException, Throwable {
-		
-		return null;
+		ClientProxy clientProxy = mapaServicos.get(serviceName);
+		return clientProxy;
 	}
 
-	
+
 	public List<String> list() throws UnknownHostException, IOException, Throwable {
-		
-		return null;
+		List<String> keys = new ArrayList<>();
+		Set<String> chaves = mapaServicos.keySet();		
+		for(String chave: chaves)
+			keys.add(chave);
+		return keys;
 	}
-	
+
 
 }
